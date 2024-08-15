f33599803e78bb8489fdda5abbf4aba9d6e7c8f8
Gabriela <gabifmouraroc@outlook.com>
2024-08-15 10:14:08 -0300

    fetch de categorias

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,34 @@
 import styles from "./page.module.css";
 import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
-import { getCategorias, getTodosProdutos } from "../lib/api";
 
 async function fetchProdutosApi() {
-  const res = await fetch("http://localhost:3000/api/produtos")
+  const res = await fetch("http://localhost:3000/api/produtos");
 
-  if(!res.ok) {
-    throw new Error("Não foi possível obter os dados")
+  if (!res.ok) {
+    throw new Error("Não foi possível obter os dados");
   }
 
-  const produtos = await res.json()
+  const produtos = await res.json();
 
-  return produtos
+  return produtos;
+}
+
+async function fetchCategoriasApi() {
+  const res = await fetch("http://localhost:3000/api/categorias");
+
+  if (!res.ok) {
+    throw new Error("Categorias não encontradas");
+  }
+
+  const categorias = await res.json();
+
+  return categorias;
 }
 
 export default async function Home() {
   const { produtos } = await fetchProdutosApi();
-  const categorias = getCategorias();
+  const { categorias } = await fetchCategoriasApi();
 
   return (
     <>
