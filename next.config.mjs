ca536c1c35d219c3ab9476083e45d718f098f14c
Gabriela <gabifmouraroc@outlook.com>
2024-08-17 11:11:12 -0300

    testando arquivos estáticos

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-        pathname: "**",
-      },
-    ],
+    loader: 'custom',
+    loaderFile: './src/app/imagem.js',
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "raw.githubusercontent.com",
+    //     pathname: "**",
+    //   },
+    // ],
   },
 };
 
